c62ed6a91b033cf1733a1730a2f1aa990f8579b9
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2025-02-02 01:33:39 +0530

    Implement a program to find the difference between two sets

---
+++ Task 8/8.10.py
@@ -0,0 +1,9 @@
+# Define two sets
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+# Find the difference
+difference = set1.difference(set2)
+
+# Print the difference
+print(f"Difference between set1 and set2: {difference}")
