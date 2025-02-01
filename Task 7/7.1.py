9c212f889ec4a9c0d33ad088a0d15595d0cf4acf
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2025-02-01 22:26:21 +0530

    Create a list and append, remove, and sort elements

---
+++ Task 7/7.1.py
@@ -0,0 +1,19 @@
+my_list = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+
+
+# Append the number 11 to the end of the list
+my_list.append(11)
+
+# Append the number 0 to the beginning of the list
+my_list.insert(0, 0)
+
+# Remove the number 3 from the list
+my_list.remove(3)
+
+# Sort the list
+my_list.sort()
+
+# Reverse the list
+my_list.reverse()
+
+print(my_list)
