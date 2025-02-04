4457dd66fbf8ecbef9e4340b372704363815e5aa
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2025-02-04 21:58:59 +0530

    Create a function that raises a ValueError if a negative number is passed.

--- Task 12/12.3.py
+++ Task 12/12.3.py
@@ -1,14 +1,9 @@
-# Create a function that raises a ValueError if a negative number is passed.
+def check_positive_number(number):
+    if number < 0:
+        raise ValueError("Negative numbers are not allowed!")
+    else:
+        print(f"The number {number} is valid.")
 
-def raise_value_error(n):
-    try:
-        if n < 0:
-            raise ValueError("Negative number passed")
-        
-        return n
-    except ValueError as e:
-        return e
-    
 
-value = input("Enter a number: ")
-print(raise_value_error(value))
+
+value = int(input("Enter a number: "))
